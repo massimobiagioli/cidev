e3e53092300a3a78e086555bfd50f1f5d0857610
Massimo Biagioli <biagiolimassimo@gmail.com>
2016-11-08 22:47:54 +0100

    Login Admin Form

--- application/modules/admin/views/login_admin_view.php
+++ application/modules/admin/views/login_admin_view.php
@@ -6,12 +6,59 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
     <title>Login</title>
     <?=$includes?>
+    <style>
+        .container {
+            width: 750px;
+            padding-right: 15px;
+            padding-left: 15px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+        .form-signin-heading {
+            margin-bottom: 10px;
+            font-size: 24px;
+        }
+        .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+        }
+        .sr-only {
+            position: absolute;
+            width: 1px;
+            height: 1px;
+            padding: 0;
+            margin: -1px;
+            overflow: hidden;
+            clip: rect(0,0,0,0);
+            border: 0;
+        }
+        .form-control {
+            margin-bottom: 5px;
+        }
+    </style>
 </head>
-<body>
-    <p-splitbutton icon="fa-check" label="Save" name="save-btn">
-        <p-menuitem icon="fa-edit" text="Update"></p-menuitem>
-        <p-menuitem icon="fa-minus" text="Delete"></p-menuitem>
-        <p-menuitem icon="fa-home" text="Homepage"></p-menuitem>
-    </p-splitbutton>
+<body>    
+    <div class="container">
+        <form class="form-signin">
+            <div class="form-control" align="center">
+                <h2 class="form-signin-heading ui-widget">Backend Login</h2>
+            </div>
+            
+            <div class="form-control" align="center">
+                <label for="username" class="sr-only">Username</label>
+                <input type="text" is="p-inputtext" value="" placeholder="Username" width="30" required autofocus/>
+            </div>
+          
+            <div class="form-control" align="center">
+                <label for="password" class="sr-only">Password</label>
+                <input type="password" is="p-password" id="password" placeholder="Password" width="30" required />
+            </div>
+            
+            <div class="form-control" align="center">
+                <button is="p-button" type="submit">Login</button>
+            </div>                
+        </form>
+    </div>
 </body>
 </html>
