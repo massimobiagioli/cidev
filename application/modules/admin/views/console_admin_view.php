847b8e49f079516374bdb1408cc851d1332ddc3c
Massimo Biagioli <biagiolimassimo@gmail.com>
2016-11-19 18:51:41 +0100

    Console di amministrazione con cd-panelmenu

--- application/modules/admin/views/console_admin_view.php
+++ application/modules/admin/views/console_admin_view.php
@@ -19,32 +19,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <div id="console-main-content"></div>
         </main>
         <nav class="console-nav">                        
-            <div id="sidebar" style="width:300px">
-                <div>
-                    <div><a id="menu-api-keys" data-icon="fa-key"><?=$this->lang->line('api_keys')?></a></div>
-                    <div>
-                        <ul>
-                            <li><a data-icon="fa-bank"><?=$this->lang->line('gestione_api_keys')?></a></li>                            
-                        </ul>
-                    </div>                    
-                </div>
-                <div>
-                    <div><a id="menu-action" data-icon="fa-gear"><?=$this->lang->line('azioni')?></a></div>
-                    <div>
-                        <ul>
-                            <li><a data-icon="fa-close"><?=$this->lang->line('esci')?></a></li>                            
-                        </ul>
-                    </div>        
-                </div>
-            </div>                        
+            <cd-panelmenu id="sidebar" width="300px">
+                <cd-panelmenuitem id="apikeys" icon="fa-key" expanded="true"><?=$this->lang->line('api_keys')?>
+                    <cd-panelmenusubitem id="apikeys-handler" href="#" icon="fa-bank"><?=$this->lang->line('gestione_api_keys')?></cd-panelmenusubitem>
+                </cd-panelmenuitem>
+                <cd-panelmenuitem id="actions" icon="fa-gear" expanded="true"><?=$this->lang->line('azioni')?>
+                    <cd-panelmenusubitem id="action-exit" href="#" icon="fa-close"><?=$this->lang->line('esci')?></cd-panelmenusubitem>
+                </cd-panelmenuitem>
+            </cd-panelmenu>                        
         </nav>
     </div>
-    
-    <script>
-        $('#sidebar').puipanelmenu();
-        $('#menu-api-keys').trigger('click');
-        $('#menu-action').trigger('click');
-    </script>
-    
 </body>
 </html>
